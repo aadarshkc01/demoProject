fd244059b03c63e2f42953abf5c239a42e1da57d
Aadarsh Kc <aadarshkc2061@gmail.com>
2025-06-09 00:52:03 +0545

    Added cors for frontend

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const bookRoute = require ("./routes/bookRoute")
+const cors = require("cors")
 
 const app = express()
 require("./database/connection")
+
+app.use(cors({
+    origin : "http://localhost:5173"
+}))
 
 app.use(express.json())
 app.use("/api",bookRoute)
